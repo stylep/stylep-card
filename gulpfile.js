6aca2b2c81e6401b5ce88363229bf9a3546e2843
Stephen Way <way.stephen@gmail.com>
2015-12-28 14:18:18 -0800

    Fixed stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
-var variables = require('postcss-css-variables');
 var cssimport = require('postcss-import');
+var mixins = require('postcss-mixins');
 var nested = require('postcss-nested');
 var extend = require('postcss-extend');
 var stylelint = require('stylelint');
 var reporter = require('postcss-reporter');
 
-// Code style rules
-var stylelintConfig = {
-  "extends": "stylelint-config-suitcss",
-  "rules": {
-    "at-rule-empty-line-before": null,
-    "block-opening-brace-newline-after": null,
-    "comment-empty-line-before": null,
-    "number-leading-zero": null,
-    "string-quotes": "single",
-    "selector-list-comma-newline-after": "always-multi-line",
-    "selector-combinator-space-before": null
-  }
-}
-
 // Check code style of stylesheets
 gulp.task('css:lint', function() {
         return gulp.src('index.css')
         .pipe(postcss([
-                stylelint(stylelintConfig),
+                stylelint,
                 reporter({
                         clearMessages: true,
                 }),
@@ -38,9 +24,9 @@ gulp.task('css:lint', function() {
 gulp.task('css', ['css:lint'], function() {
         var processors = [
                 cssimport,
-                variables,
                 extend,
                 nested,
+                mixins,
                 autoprefixer(['last 2 versions'])
         ];
         return gulp.src('example/index.css')
